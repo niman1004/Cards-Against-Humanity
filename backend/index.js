1fde8e21c505b5af6f4a3ccbd87bc5cfda9a51bb
nimanpreet kaur <nimanpreetkaurbhatia@gmail.com>
2025-11-02 14:25:35 +0530

    backend logic change

--- backend/index.js
+++ backend/index.js
@@ -3,21 +3,25 @@ import { Server } from "socket.io";
 import { Player, Room } from "./entities.js";
 import express from "express"
 import dotenv from "dotenv";
+import crypto from "crypto";
 dotenv.config();
 
 
 const app = express()
 app.use(express.static("public"));
 
-const PORT= process.env.PORT || 10000
+const PORT = process.env.PORT || 10000
 
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin:process.env.FRONTEND_URL,
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST"],
     credentials: true
-  }
+  },
+  transports: ["websocket" , "polling"],
+  pingInterval: 25000,
+  pingTimeout: 60000
 });
 
 const rooms = {};
@@ -28,17 +32,34 @@ io.on("connection", (socket) => {
   console.log("User connected", socket.id);
 
   // player joins a room
-  socket.on("joinRoom", ({ roomCode, username }) => {
+  socket.on("joinRoom", ({ roomCode, username, savedPlayerId }) => {
     if (!rooms[roomCode]) {
       rooms[roomCode] = new Room(roomCode);
     }
 
-    const player = new Player(socket.id, username);
+    const room = rooms[roomCode];
+
+    if (savedPlayerId) {
+      const existingPlayer = room.players.find(p => p.playerId === savedPlayerId);
+      if (existingPlayer) {
+        if (existingPlayer.disconnectTimeout) {
+          clearTimeout(existingPlayer.disconnectTimeout);
+          existingPlayer.disconnectTimeout = null;
+        }
+
+        existingPlayer.socketId = socket.id;
+        socket.join(roomCode);
+        io.to(roomCode).emit("playerListUpdate", room.players)
+        return;
+      }
+    }
+    const playerId = crypto.randomUUID();
+    const player = new Player(playerId, socket.id, username);
     rooms[roomCode].addPlayer(player);
 
     socket.join(roomCode);
-
-    io.to(roomCode).emit("playerListUpdate", rooms[roomCode].players);
+    socket.emit("savePlayerId", playerId);
+    io.to(roomCode).emit("playerListUpdate", room.players);
   });
 
   // start game
@@ -49,7 +70,8 @@ io.on("connection", (socket) => {
     io.to(roomCode).emit("newRound", {
       blackCard: room.currBlack,
       players: room.players.map((p) => ({
-        id: p.id,
+        playerId: p.playerId,
+        socketId: p.socketId,
         name: p.name,
         isCzar: p.isCzar,
       })),
@@ -57,23 +79,23 @@ io.on("connection", (socket) => {
 
     // send private hands
     room.players.forEach((p) => {
-      io.to(p.id).emit("updateHand", p.hand);
+      io.to(p.socketId).emit("updateHand", p.hand);
     });
   });
 
   // submit card
   socket.on("submitCard", ({ roomCode, card }) => {
     const room = rooms[roomCode];
-    const player = room.players.find((p) => p.id === socket.id);
+    const player = room.players.find((p) => p.socketId === socket.id);
 
     const index = player.hand.indexOf(card);
     if (index !== -1) player.hand.splice(index, 1);
 
-    room.submissions.push({ card, playerId: player.id });
+    room.submissions.push({ card, socketId: player.socketId });
 
     // give new card
     player.hand.push(room.whiteDeck.pop());
-    io.to(player.id).emit("updateHand", player.hand);
+    io.to(player.socketId).emit("updateHand", player.hand);
 
     //brodcasting submission process 
     io.to(roomCode).emit("submissionsUpdate", room.submissions)
@@ -81,19 +103,21 @@ io.on("connection", (socket) => {
     // check if all submitted
     if (room.submissions.length === room.players.length - 1) {
       const czar = room.players.find((p) => p.isCzar);
-      io.to(czar.id).emit("judgeRound", room.submissions);
+      io.to(czar.socketId).emit("judgeRound", room.submissions);
     }
   });
 
   // czar picks winner
-  socket.on("pickWinner", ({ roomCode, winnerId }) => {
+  socket.on("pickWinner", ({ roomCode, winnerId, winningCard }) => {
     const room = rooms[roomCode];
-    const winner = room.players.find((p) => p.id === winnerId);
+    const winner = room.players.find((p) => p.socketId === winnerId);
     winner.score++;
 
     io.to(roomCode).emit("roundResult", {
       winner: winner.name,
       scores: room.players.map((p) => ({ name: p.name, score: p.score })),
+      winningCard: winningCard
+
     });
     room.submissions = [];
     room.nextRound();
@@ -101,10 +125,10 @@ io.on("connection", (socket) => {
     io.to(roomCode).emit("newRound", {
       blackCard: room.currBlack,
       players: room.players.map((p) => ({
-        id: p.id,
+        playerId: p.playerId,
+        socketId: p.socketId,
         name: p.name,
-        isCzar: p.isCzar,
-        score: p.score
+        isCzar: p.isCzar
       })),
     });
   });
@@ -114,19 +138,25 @@ io.on("connection", (socket) => {
     //finding room to which this socket belonged to
     for (const roomCode in rooms) {
       const room = rooms[roomCode];
-      const index = room.players.findIndex((p) => p.id === socket.id);
+      const player = room.players.find((p) => p.socketId === socket.id);
+      if (!player) continue;
 
-      if (index !== -1) {
-        room.players.splice(index, 1);
-        io.to(roomCode).emit("playerListUpdate", room.players)
-      }
-      if (room.players.length === 0) {
-        delete rooms[roomCode]
-      }
+      player.disconnectTimeout = setTimeout(() => {
+        room.players = room.players.filter(p => p.playerId !== player.playerId);
+        io.to(roomCode).emit("playerListUpdate", room.players);
 
+        if (room.players.length === 0) {
+          delete rooms[roomCode];
+        }
+
+        console.log("Removed player after timeout::", player.name);
+
+      }, 6000);
       break;
     }
   })
+
+
 });
 
 httpServer.listen(PORT, () => {
